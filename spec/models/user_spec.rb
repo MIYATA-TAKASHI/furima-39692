b7387aaaf026e3c990c76482fc58494ab67659d1
MIYATA-TAKASHI <kulando000@gmail.com>
2023-08-23 17:51:57 +0900

    単体テスト　修正2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,111 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'ユーザー新規登録' do
-    it '正常な値で登録できること' do
-      user = build(:user,
-                   nickname: 'test_user',
-                   email: 'test@example.com',
-                   last_name: '山田',
-                   first_name: '太郎',
-                   kana_last_name: 'ヤマダ',
-                   kana_first_name: 'タロウ',
-                   birthdate: Date.new(1990, 1, 1),
-                   password: 'Password123'
-      )
-      expect(user).to be_valid
-    end
+  before do
+    @user = FactoryBot.build(:user)
   end
 
   describe 'ユーザー新規登録' do
-    context '新規登録できる場合' do
-      before do
-        @user = build(:user)
-      end
-
+    context '新規登録できるとき' do
       it 'nicknameが入力されていれば登録できる' do
-        expect(@user).to be_valid
       end
-
       it 'last_nameが正しいフォーマットであれば登録できる' do
-        expect(@user).to be_valid
       end
-
       it 'first_nameが正しいフォーマットであれば登録できる' do
-        expect(@user).to be_valid
       end
-
       it 'kana_last_nameが正しいフォーマットであれば登録できる' do
-        expect(@user).to be_valid
       end
-
       it 'kana_first_nameが正しいフォーマットであれば登録できる' do
-        expect(@user).to be_valid
       end
-
       it 'birthdateが正しく選択されていれば登録できる' do
-        expect(@user).to be_valid
+      end
+      it 'パスワードが6文字以上かつ半角英数字混合であれば登録できる' do
       end
     end
-
     context '新規登録できない場合' do
-      before do
-        @user = build(:user)
-      end
-
-      it 'nicknameが空では登録できない' do
-        @user.nickname = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
-      end
-
-      it 'emailが空では登録できない' do
-        @user.email = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Email can't be blank")
-      end
-
       it 'emailが一意でなければ登録できない' do
-        existing_user = create(:user, email: 'test@example.com') # 既存ユーザーの作成
-        @user.email = existing_user.email
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Email has already been taken')
       end
-
       it 'emailに＠が含まれていなければ登録できない' do
-        @user.email = 'invalid_email'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Email is invalid')
       end
-
       it 'last_nameが正しいフォーマットでなければ登録できない' do
-        @user.last_name = '123'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Last name is invalid')
       end
-
       it 'first_nameが正しいフォーマットでなければ登録できない' do
-        @user.first_name = '123'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('First name is invalid')
       end
-
       it 'kana_last_nameが正しいフォーマットでなければ登録できない' do
-        @user.kana_last_name = 'かな'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Kana last name is invalid')
       end
-
       it 'kana_first_nameが正しいフォーマットでなければ登録できない' do
-        @user.kana_first_name = 'かな'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Kana first name is invalid')
       end
-
       it 'birthdateが空では登録できない' do
-        @user.birthdate = nil
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Birthdate can't be blank")
+      end
+      it 'パスワードが6文字以上かつ半角英数字混合でなければ登録できない' do
+      end
+      it 'パスワードが空では登録できない' do
+      end
+      it '姓（全角）が空だと登録できない' do
+      end
+      it '名（全角）が空だと登録できない' do
+      end
+      it '姓（カナ）が空だと登録できない' do
+      end
+      it '名（カナ）が空だと登録できない' do
+      end
+      it 'パスワードが6文字未満では登録できない' do
+      end
+      it '英字のみのパスワードでは登録できない' do
+      end
+      it '数字のみのパスワードでは登録できない' do
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+      end
+      it 'パスワードとパスワード（確認用）が不一致だと登録できない' do
       end
     end
   end
