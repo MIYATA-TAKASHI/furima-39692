304686d8697c6b65f3abdde67d96449a96923392
MIYATA-TAKASHI <kulando000@gmail.com>
2023-08-28 23:06:11 +0900

    indexの認証を削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :items, only: [:index, :new, :create]
  
-  get 'items', to: 'items#index'
+  # ユーザーのプロフィール表示
   get '/users/:id', to: 'users#show', as: :user
 
   root to: "items#index"
-  
-
 end
