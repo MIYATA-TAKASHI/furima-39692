a9032e7ce745e359ee139378694fcdcd668db8c4
MIYATA-TAKASHI <kulando000@gmail.com>
2023-08-29 21:27:49 +0900

    商品出品機能　修正2

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,14 +3,14 @@ window.addEventListener('turbo:load', () => {
 
 
 const priceInput = document.getElementById("item-price");
-console.log(priceInput);
+
 
 const addTaxDom = document.getElementById("add-tax-price");
 const profitDom = document.getElementById("profit");
 
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  console.log("イベント発火");
+
 
   const tax = Math.floor(inputValue * 0.1);
   const profit = inputValue - tax;
--- db/migrate/20230824075946_create_items.rb
+++ db/migrate/20230824075946_create_items.rb
@@ -2,10 +2,10 @@ class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :name
+      t.string :name, null: false
       t.integer :category_id, null: false, foreign_key: true
-      t.text :description
-      t.integer :price
+      t.text :description, null: false
+      t.integer :price, null: false
       t.integer :status_id, null: false, foreign_key: true
       t.integer :delivery_fee_burden_id, null: false, foreign_key: true
       t.integer :prefecture_id, null: false, foreign_key: true
@@ -15,3 +15,4 @@ class CreateItems < ActiveRecord::Migration[7.0]
     end
   end
 end
+
