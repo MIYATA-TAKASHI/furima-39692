97a081fab808a86527bb0bb465df4b09bab2895e
MIYATA-TAKASHI <kulando000@gmail.com>
2023-08-30 22:12:20 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :items, only: [:index, :new, :create]
- 
-  # ユーザーのプロフィール表示
-  get '/users/:id', to: 'users#show', as: :user
+  resources :items, only: [:index, :new, :create, :show]
 
   root to: "items#index"
 end
