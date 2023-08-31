ac459a8cd24c0f06f543b986f685b38def55bfbf
MIYATA-TAKASHI <kulando000@gmail.com>
2023-08-31 12:28:41 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
-  resources :items, only: [:index, :new, :create, :show]
+  devise_for :users 
+  # controllers: { registrations: 'registrations' }
+  resources :items, only: [:index, :new, :create, :show, :edit]
+
+  resources :items do
+    resources :purchases
+  end
+
 
   root to: "items#index"
 end
