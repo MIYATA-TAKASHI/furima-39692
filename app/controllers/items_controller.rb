125cd8eba24b4b602d6b78ba62e6d74fd5ddb283
MIYATA-TAKASHI <kulando000@gmail.com>
2023-09-01 16:21:51 +0900

    コードレビュー後　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,13 +14,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    if current_user.id == @item.user_id
-      @item.destroy
-      redirect_to root_path
-    else
-      render :show
-    end
+    @item.destroy
+    redirect_to root_path
   end
 
   def update
