b5a66e0da0cdadb068f9623a6d6221eb92b1f60a
MIYATA-TAKASHI <kulando000@gmail.com>
2023-09-10 15:12:19 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.17.3'
 set :application, 'furima-39692'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:miyata-takashi/.furima-39692git'
+set :repo_url,  'git@github.com:miyata-takashi/furima-39692.git'
 set :branch, 'main'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
