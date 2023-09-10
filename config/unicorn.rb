cb351aa01c47ddfd44ec7c3bd0dfb0684a27fbc0
MIYATA-TAKASHI <kulando000@gmail.com>
2023-09-10 14:37:46 +0900

    修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,11 +7,13 @@ worker_processes 1
 #アプリケーションの設置されているディレクトリを指定
 working_directory "#{app_path}/current"  # 「current」を指定
 
+
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"  # 「shared」の中を参照するよう変更
 
 #ポート番号を指定
 listen "#{app_path}/tmp/sockets/unicorn.sock"
+
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"  # 「shared」の中を参照するよう変更
 
